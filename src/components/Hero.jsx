79136384c503410e99e835dda618c066bc9d8af0
Ajit Kushwaha <ajitkushwahaswe@gmail.com>
2024-05-29 08:48:20 +0530

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ import Button from './Button'
 const Hero = () => {
   return (
     <div className='sm:h-screen bg-pattern4 bg-no-repeat bg-cover flex flex-col md:flex-row justify-center items-center w-full'>
-       <div className='w-[50%] h-screen md:px-[50px] flex justify-center flex-col items-center'>
+       <div className='w-[50%] ml-[100px] h-screen md:px-[100px] flex justify-center flex-col items-center'>
        {/* <img className='w-[300px]' src={chef}/> */}
           <h2 className='md:text-[80px] text-[28px] font-poppins font-bold'> Tasty Food </h2>
           <h2 className={`md:text-[80px] text-[40px] font-poppins font-bold text-primary`}> 
@@ -16,6 +16,7 @@ const Hero = () => {
 
       <p className='md:px-[60px] font-poppins py-[30px] text-center'>To Provide nutritional support for an orgnisation . Food is usually of plant , animal or fungal origin...</p>
 
+      <a href='#franchise'>
       <Button className='text-center items-center'
                     // func={"sumbitHandler"}
                    title={"GET FRANCHISE"}
@@ -27,15 +28,16 @@ const Hero = () => {
                     bgHover={"white"}
                     hoverBorder={"primary"}
                    />
+      </a>
        </div>
 
 
       
        <div className='w-[60%] bg-pattern2 flex justify-center items-center'>
-          <img className='absolute w-[150px] sm:w-[220px] sm:top-16 top-20 right-0' src={tomato}/>
-          <img className='w-[600px]' src={pizza2}/>
+          <img className='absolute w-[150px] sm:w-[270px] sm:h-270px sm:top-16 top-20 right-0' src={tomato}/>
+          <img className='w-[800px] h-[600px] mr-[150px]' src={pizza2}/>
 
-          <img className='absolute w-[150px] sm:w-[220px] bottom-10 left-0' src={mirch}/> 
+          <img className='absolute w-[150px] sm:w-[220px] bottom-10 left-10' src={mirch}/> 
        </div>
     </div>
   ) 
